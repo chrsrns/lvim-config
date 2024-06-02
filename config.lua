408812f7ee84f7009ffbae53ede9297cfe6976de
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-02 15:22:23 +0800

    Modified minimap config
    This fix will show the minimap on startup. The recipe on LazyVim's docs don't work.

--- config.lua
+++ config.lua
@@ -25,11 +25,12 @@ lvim.plugins = {
   {
     'wfxr/minimap.vim',
     build = "cargo install --locked code-minimap",
-    -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-    config = function()
-      vim.cmd("let g:minimap_width = 10")
-      vim.cmd("let g:minimap_auto_start = 1")
-      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    lazy = false,
+    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+    init = function()
+      vim.g.minimap_width = 10
+      vim.g.minimap_auto_start = 1
+      vim.g.minimap_auto_start_win_enter = 1
     end,
   },
   {
