e8819ae1adab9cc8f495b9fcdb840f705db46a3e
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-02 14:31:46 +0800

    Updated import config of neo-tree

--- config.lua
+++ config.lua
@@ -71,35 +71,11 @@ lvim.plugins = {
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("neo-tree").setup({
-        close_if_last_window = true,
-        window = {
-          width = 30,
-        },
-        buffers = {
-          follow_current_file = true,
-        },
-        filesystem = {
-          follow_current_file = true,
-          filtered_items = {
-            hide_dotfiles = false,
-            hide_gitignored = false,
-            hide_by_name = {
-              "node_modules"
-            },
-            never_show = {
-              ".DS_Store",
-              "thumbs.db"
-            },
-          },
-        },
-      })
-    end
-  },
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
 }
 
 lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
