6abb4aaf69165a5f1a5f719fc4a72b12f65067de
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-06 19:06:06 +0800

    Do not auto opem the minimap

--- config.lua
+++ config.lua
@@ -29,8 +29,8 @@ lvim.plugins = {
     cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
     init = function()
       vim.g.minimap_width = 10
-      vim.g.minimap_auto_start = 1
-      vim.g.minimap_auto_start_win_enter = 1
+      vim.g.minimap_auto_start = 0
+      vim.g.minimap_auto_start_win_enter = 0
       vim.g.minimap_highlight_range = true
       vim.g.minimap_highlight_search = true
     end,
