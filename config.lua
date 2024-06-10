c6666a6a16b9933cb501600a108e15eb67c91884
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-10 23:27:48 +0800

    Added new plugins
    Added conform.nvim. I've yet to configure this.
    Added precognition.nvim for motion hints.
    Updated some plugins on lazy-lock.json.

--- config.lua
+++ config.lua
@@ -94,6 +94,37 @@ lvim.plugins = {
       "MunifTanjim/nui.nvim",
       "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
     }
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {},
+  },
+  {
+    "tris203/precognition.nvim",
+    --event = "VeryLazy",
+    config = {
+      -- startVisible = true,
+      -- showBlankVirtLine = true,
+      -- highlightColor = { link = "Comment" },
+      -- hints = {
+      --      Caret = { text = "^", prio = 2 },
+      --      Dollar = { text = "$", prio = 1 },
+      --      MatchingPair = { text = "%", prio = 5 },
+      --      Zero = { text = "0", prio = 1 },
+      --      w = { text = "w", prio = 10 },
+      --      b = { text = "b", prio = 9 },
+      --      e = { text = "e", prio = 8 },
+      --      W = { text = "W", prio = 7 },
+      --      B = { text = "B", prio = 6 },
+      --      E = { text = "E", prio = 5 },
+      -- },
+      -- gutterHints = {
+      --     G = { text = "G", prio = 10 },
+      --     gg = { text = "gg", prio = 9 },
+      --     PrevParagraph = { text = "{", prio = 8 },
+      --     NextParagraph = { text = "}", prio = 8 },
+      -- },
+    },
   }
 }
 
