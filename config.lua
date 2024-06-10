40d59acf21866470e5a45f7be5d00fc5f5725696
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-11 07:46:31 +0800

    Moved telescope config to `telescope.lua`

--- config.lua
+++ config.lua
@@ -6,6 +6,7 @@
 -- Import plugins from plugin module
 require("plugins")
 require("base")
+require("telescope")
 
 lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
 lvim.builtin.which_key.mappings["e"] = {
@@ -59,97 +60,6 @@ formatters.setup {
 
 -- Added tailwindcss LSP
 require("lvim.lsp.manager").setup("tailwindcss")
-
--- Added explicit list for telescope to ignore
-lvim.builtin.telescope.defaults.file_ignore_patterns = {
-  "^po/*",
-  "vendor/*",
-  "%.lock",
-  "__pycache__/*",
-  "%.sqlite3",
-  "%.ipynb",
-  "node_modules/*",
-  "%.jpg",
-  "%.jpeg",
-  "%.png",
-  "%.svg",
-  "%.otf",
-  "%.ttf",
-  ".git/",
-  "%.webp",
-  ".dart_tool/",
-  ".github/",
-  ".gradle/",
-  ".idea/",
-  ".settings/",
-  ".vscode/",
-  "__pycache__/",
-  "build/",
-  "env/",
-  "gradle/",
-  "node_modules/",
-  "target/",
-  "%.pdb",
-  "%.dll",
-  "%.class",
-  "%.exe",
-  "%.cache",
-  "%.ico",
-  "%.pdf",
-  "%.dylib",
-  "%.jar",
-  "%.docx",
-  "%.met",
-  "%.po",
-  "smalljre_*/*",
-  ".vale/",
-}
-
--- Changed telescope layout (text search, etc)
-lvim.builtin.telescope.defaults.layout_config = {
-  prompt_position = "top",
-  height = 0.9,
-  width = 0.9,
-  bottom_pane = {
-    height = 25,
-    preview_cutoff = 120,
-  },
-  center = {
-    height = 0.4,
-    preview_cutoff = 40,
-    width = 0.5,
-  },
-  cursor = {
-    preview_cutoff = 40,
-  },
-  horizontal = {
-    preview_cutoff = 120,
-    preview_width = 0.6,
-  },
-  vertical = {
-    preview_cutoff = 40,
-  },
-  flex = {
-    flip_columns = 150,
-  },
-}
-for key, _ in pairs(lvim.builtin.telescope.pickers) do
-  if key ~= "planets" then
-    lvim.builtin.telescope.pickers[key].previewer = nil
-    lvim.builtin.telescope.pickers[key].theme = nil
-    lvim.builtin.telescope.pickers[key].layout_strategy = nil
-  end
-end
-lvim.builtin.telescope.pickers.git_files.previewer = nil
-lvim.builtin.telescope.defaults.layout_strategy = "flex"
-lvim.builtin.telescope.defaults.prompt_prefix = "  "
-lvim.builtin.telescope.defaults.selection_caret = "❯ "
-lvim.builtin.telescope.defaults.winblend = 10
-lvim.builtin.telescope.defaults.borderchars = {
-  prompt = { " ", " ", " ", " ", " ", " ", " ", " " },
-  results = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-  preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-}
 
 -- Added linter for typscript and javascript
 local linters = require "lvim.lsp.null-ls.linters"
