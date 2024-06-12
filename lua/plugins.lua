bb6d99d8f78d13520ea5ca0833b423c170ae6201
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-12 20:04:12 +0800

    Commented out some `leap.nvim` remaps
    Some remaps are conflicting with the surround plugin.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,8 +76,8 @@ lvim.plugins = {
     config = function()
       vim.keymap.set({ 'n', 'v' }, 's', '<Plug>(leap)')
       vim.keymap.set({ 'n', 'v' }, 'S', '<Plug>(leap-from-window)')
-      vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
-      vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+      -- vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
+      -- vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
     end,
   },
   {
