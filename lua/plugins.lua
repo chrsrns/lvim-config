d5ab67b1141e0d3977b713098e831a4c25d401f8
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-13 11:16:00 +0800

    Remove deprecated module argument

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -178,7 +178,6 @@ lvim.plugins = {
   {
     "folke/persistence.nvim",
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
-    module = "persistence",
     config = function()
       require("persistence").setup {
         dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
